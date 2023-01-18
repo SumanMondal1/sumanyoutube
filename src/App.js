cd4f4ca3f8990dc49c889e2603c7d7117ca68790
Soumik Maity <soumik.maity@geogo.in>
2023-01-18 22:00:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,34 +4,51 @@ import Header from './component/Header';
 import Sidebar from './component/Sidebar';
 import SearchPage from "./component/SearchPage";
 import RecommendedVideos from './component/RecommendedVideos';
-import { BrowserRouter as Router ,Route ,Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Abc } from './component/Abc';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Header />
+        <Header />
         <Routes>
-         <Route path="/search/:searchTerm"
-          element={<div className="app_sidebar">
-          <Sidebar />
-          <SearchPage />
-          </div>} />
+          <Route path="/search/:searchTerm"
+            element={<div className="app_sidebar">
+              <Sidebar />
+              <SearchPage />
+            </div>} />
           <Route path="/" element={
-      <div className="app_sidebar">
-      <Sidebar />
-      <RecommendedVideos />
-      <div>
-      <div>
-    </div>
-    </div>
-      </div>} />
-        
+            <div className="app_sidebar">
+              <Sidebar />
+              <RecommendedVideos />
+              <div>
+                <div>
+                </div>
+              </div>
+            </div>} />
+
+          <Route path="/videos" element={
+            <div className="app_sidebar">
+              <Sidebar />
+              <RecommendedVideos />
+              <div>
+                <div>
+                </div>
+              </div>
+            </div>} />
+
+          <Route path="/videos/:id" element={
+            <div className="app_sidebar">
+              <Sidebar />
+              <Abc />
+            </div>} />
+
         </Routes>
       </Router>
-      
-      
+
+
     </div>
   );
 }
